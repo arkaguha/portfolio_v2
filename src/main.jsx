e445edc7587234f8621bdbe1cbf7867cba342c29
Arka Guha <arkaguha999@gmail.com>
2025-09-15 12:47:07 +0530

    fixed invalid import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import Portfolio from "./portfolio_wireframe";
-import Portfolio from "./components/portfolio";
+// import Portfolio from "./components/portfolio";
+import Portfolio from "./components/Portfolio";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
