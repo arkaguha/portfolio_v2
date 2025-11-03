531ccb62ce82f35ea223f3e7dc92ac1ca18ee29f
arkaguha <97331568+arkaguha@users.noreply.github.com>
2025-11-03 13:09:01 +0530

    Update resume link to new PDF file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero({ darkMode }) {
               View Projects
             </a>
             <a
-              href="/resume/14thOct2025.pdf"
+              href="/resume/03rdNov2025.pdf"
               target="_blank"
               className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
             >
